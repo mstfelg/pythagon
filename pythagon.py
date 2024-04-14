930bc324daf8dde4a53dc97deb0e2d0e01ba708f
Mustafa Alelg <me@mstfelg.com>
2024-04-13 22:25:45 -0700

    Add test case

--- pythagon.py
+++ pythagon.py
@@ -1,20 +1,19 @@
-import sympy
-from sympy import acos, cos, pi, sin, sqrt
 from sympy.geometry import *
+from sympy import acos, cos, pi, sin, sqrt
 
 def expi(angle):
     return Point(cos(angle), sin(angle))
 
-def triangle(O, b, A, c):
+def triangle(O, b, alpha, c):
 
     # SAS
-    a = sqrt(b*b+c*c-2*b*c*cos(A))
-    R = a/(2*sin(A))
+    a = sqrt(b*b+c*c-2*b*c*cos(alpha))
+    R = a/(2*sin(alpha))
     beta1 = acos((c*c+a*a-b*b)/(2*c*a))
-    gamma1 = pi - A - beta1
+    gamma1 = pi - alpha - beta1
 
-    A = O + Point(1,0)*Point(0,R)
-    B = O + expi(A)*Point(0,-R)
-    C = O + expi(-A)*Point(0,-R)
+    A = O + Point(1,0) * Point(0,R)
+    B = O + expi(alpha) * Point(0,-R)
+    C = O + expi(-alpha) * Point(0,-R)
 
     return [A,B,C]
